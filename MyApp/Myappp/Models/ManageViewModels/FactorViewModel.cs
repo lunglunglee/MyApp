0d235647a22ff1dd3b613ad89a1c84e08b91cce1
donlee <lunglungleelee@hotmail.com>
2016-06-08 01:07:34 +0800

    g

---
+++ MyApp/Myappp/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Myappp.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
